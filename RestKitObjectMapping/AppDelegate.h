24160936c63b099050c1bc33326d784d05509d33
Arthi P <parthi123@icloud.com>
2015-12-18 11:24:28 -0500

    Initial Commit

---
+++ RestKitObjectMapping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestKitObjectMapping
+//
+//  Created by Arthi on 12/18/15.
+//  Copyright © 2015 Arthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
